352e5439f8cd96e4b9c838a5c1041621ce1684bf
Naveen Kumar S <156082282+Naveenr810953@users.noreply.github.com>
2026-03-21 21:48:04 +0530

    Disease Prediction.py

--- leaf_Detection_Prediction/Disease Prediction.py
+++ leaf_Detection_Prediction/Disease Prediction.py
@@ -9,7 +9,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D
 import matplotlib.pyplot as plt        
 import numpy as np  
-import os   
+import os     
       
 # --- 3. Define Parameters ---
 IMAGE_SIZE = 224    
